f3bc6935a03780716b65cea4929cbe2c2b09dba4
riinuots <riinu.pius@ed.ac.uk>
2024-12-04 12:51:02 +0000

    right answer for test, not for real input

---
+++ 04/ceres_search.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+
+len = readLines("04/input", 1) |> str_count("")
+
+m = read.fwf("04/input", widths = rep(1, len)) |> 
+  as.matrix()
+
+# Part I
+regexp = "(?=XMAS|SAMX)" # this includes overlapping instances
+n = m |>
+  diag() |>
+  paste(collapse = "") |> 
+  str_count("XMAS|SAMX")
+
+mt = t(m)
+n = n + mt |> 
+  diag() |>
+  paste(collapse = "") |> 
+  str_count("XMAS|SAMX")
+
+for(i in 1:(len-3)){
+  #print(m[, -c(1:i)])
+  # upper right diagonals
+  n = n + m[, -c(1:i)] |>
+    diag() |>
+    paste(collapse = "") |>
+    str_count(regexp)
+  # lower left diagonals
+  n = n + m[-c(1:i),] |>
+    diag() |>
+    paste(collapse = "") |>
+    str_count(regexp)
+  
+  # lower right or left, who knows what's what after t()
+  n = n + mt[, -c(1:i)] |>
+    diag() |>
+    paste(collapse = "") |>
+    str_count(regexp)
+  # lower left or right
+  n = n + mt[-c(1:i),] |>
+    diag() |>
+    paste(collapse = "") |>
+    str_count(regexp)
+}
+
+
+# rows
+n = n + m |> 
+  apply(1, paste, collapse = "") |> 
+  str_count(regexp) |> 
+  sum()
+
+# columns
+n = n + m |> 
+  apply(2, paste, collapse = "") |> 
+  str_count(regexp) |> 
+  sum()
+
+
+n
+# 2572 too low
