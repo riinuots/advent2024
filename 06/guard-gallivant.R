ca5f5c9ebf3eafaafe65e95b94fe25c5fab437b6
riinuots <riinu.pius@ed.ac.uk>
2024-12-06 15:12:56 +0000

    Day 06 part I

---
+++ 06/guard-gallivant.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+imax = 130
+jmax = 130
+
+# read.fwf but don't treat # as comments
+input_orig = read.fwf("06/input", widths = rep(1, imax), comment.char = "") |> 
+  as.matrix()
+
+loc = which(input_orig == "^", arr.ind = TRUE)
+dir = "^"
+move = list("^" = list(-1, 0), "v" = list(1, 0), "<" = list(0, -1), ">" = list(0, 1))
+path = loc
+
+while (loc[1] > 0 & loc[1] <= imax & loc[2] > 0 & loc[2] <= jmax) {
+  step_to = loc + unlist(move[dir])
+  if (input_orig[step_to] == "#") {
+    dir = c(">", "v", "<", "^")[c("^", ">", "v", "<") == dir]
+  } else {
+    path = rbind(path, step_to)
+    loc = step_to
+  }
+}
+
+path |> tibble() |> distinct() |> nrow()
